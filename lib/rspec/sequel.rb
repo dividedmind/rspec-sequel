5e9edac1c7740dacbcc1d0bf33ad7d28e88fb70d
Rafał Rzepecki <divided.mind@gmail.com>
2013-09-09 23:50:49 +0200

    Configuration fixes.

--- lib/rspec/sequel.rb
+++ lib/rspec/sequel.rb
@@ -1,10 +1,11 @@
 require "rspec/sequel/version"
 
 require "sequel"
+require "rspec/core"
 
 require "rspec/sequel/migration_example_group"
 
 RSpec::configure do |c|
   c.include RSpec::Sequel::MigrationExampleGroup, type: :migration, 
-      example_group: { file_path: %r{spec/mig} }
+      example_group: { file_path: %r{spec/migration} }
 end
