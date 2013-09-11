d30a9822e7a00094ef7856cdb51ecc19c5b9d9cf
Rafał Rzepecki <divided.mind@gmail.com>
2013-09-11 06:08:01 +0200

    Make sure there are any examples to pass.

--- features/step_definitions/examples_passing_steps.rb
+++ features/step_definitions/examples_passing_steps.rb
@@ -1,4 +1,5 @@
 Then /^the example(s)? should( all)? pass$/ do |_, _|
+  step %q{the output should match /^[^0][ 0-9]*examples?/}
   step %q{the output should contain "0 failures"}
   step %q{the exit status should be 0}
 end
